e28ad65e09ecad3a36bf60aa21cf058e18feb9e6
Canario Alan <alan.union.1998@gmail.com>
2026-01-07 10:32:35 -0300

    finalizando el ui de crear cuenta

--- views/auth/crear-cuenta.php
+++ views/auth/crear-cuenta.php
@@ -1,1 +1,49 @@
-<h1>Crear una cuenta</h1>
+<h1 class="nombre-pagina">Crear una cuenta</h1>
+<p class="descripcion-pagina">Llena el siguiente formulario para crear una cuenta</p>
+<form action="/crear-cuenta" method="post" class="formulario">
+    <div class="campo">
+        <label for="nombre">Nombre</label>
+        <input
+            type="text"
+            name="nombre"
+            id="nombre"
+            placeholder="Tu nombre">
+    </div>
+    <div class="campo">
+        <label for="apellido">Apellido</label>
+        <input
+            type="text"
+            name="apellido"
+            id="apellido"
+            placeholder="Tu apellido">
+    </div>
+    <div class="campo">
+        <label for="telefono">Teléfono</label>
+        <input
+            type="tel"
+            name="telefono"
+            id="telefono"
+            placeholder="Tu telefono">
+    </div>
+    <div class="campo">
+        <label for="email">E-mail</label>
+        <input
+            type="email"
+            name="email"
+            id="email"
+            placeholder="Tu email">
+    </div>
+    <div class="campo">
+        <label for="password">Password</label>
+        <input
+            type="password"
+            name="password"
+            id="password"
+            placeholder="Tu password">
+    </div>
+    <input type="submit" value="Crear Cuenta" class="boton">
+</form>
+<div class="acciones">
+    <a href="/">¿Ya tienes una cuenta? Inicia Sesión</a>
+    <a href="/olvide">¿Olvidaste tu Password</a>
+</div>
